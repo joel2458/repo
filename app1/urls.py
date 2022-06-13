2c395d2c75b98c42dfe992de002011b4f52c64b7
joel2458 <joeljayan2580@gmail.com>
2022-06-13 16:14:39 +0530

    second commit

--- app1/urls.py
+++ app1/urls.py
@@ -3,7 +3,10 @@ from django.urls import path,include
 from.import views
 
 urlpatterns = [
-    path('',views.base,name='base')
+    path('',views.base,name='base'),
+    path('load_stock_group',views.load_stock_group,name='load_stock_group'),
+    path('load_stock_category',views.load_stock_category,name='load_stock_category'),
+    path('load_stock_item',views.load_stock_item,name='load_stock_item',)
 
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
     return render(request,'base.html')
+
+def load_stock_group(request):
+    return render(request,'load_stock_group.html')
+    
+    
+def load_stock_category(request):
+    return render(request,'load_stock_category.html')
+
+def load_stock_item(request):
+    return render(request,'load_stock_item.html')
