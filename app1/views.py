2cdca5ffc1e290a678c924aa8bdac1b8a23cc690
joel2458 <joeljayan2580@gmail.com>
2022-06-14 16:34:39 +0530

    third commit

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def group(request):
+    return render(request,'group.html')
+
+
 def base(request):
     return render(request,'base.html')
 
